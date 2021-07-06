6eef713a274fea9fd7d8d948b127c9c51624c978
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-06 11:30:54 +0200

    je vais commencer le 8 plutot

---
+++ ex_01/Animal.php
@@ -0,0 +1,14 @@
+<?php
+class Animal {
+    private $mammal = 0;
+    private $fish = 1;
+    private $bird = 2;
+    private $name;
+    private $legs;
+    private $type;
+
+    public function __construct() {
+        
+    }
+}
+?>
